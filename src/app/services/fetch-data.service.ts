19ef0975527c4d5f336da54ff731fc880ef7ef9b
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-16 12:32:21 +0200

    Fetch Information about a Word

---
+++ src/app/services/fetch-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchDataService {
+
+  constructor(private http: HttpClient  ) { }
+
+  getWordDetails(word: string) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '03518c50f0mshb2f93a5c3fbb56fp1a5e6ajsn175d3c928506',
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+      }
+    };
+    return this.http.get('https://wordsapiv1.p.rapidapi.com/words/'+word, options)
+  }
+}
